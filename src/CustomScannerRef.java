cae655fc17cc1531530ecaff1db2ea49b24f38b3
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-05-27 10:56:46 +0200

    add gitignore

---
+++ src/CustomScannerRef.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CustomScannerRef {
+
+/*    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Jak masz na imie");
+        String name = scanner.nextLine();
+        System.out.println("Cześć " + name + " !");
+    }*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Podaj liczbę");
+        int firstNumber = scanner.nextInt();
+        int result = firstNumber * firstNumber;
+        System.out.println("Kwadrat tej liczby to " + result + " !");
+    }
+
+}
---
+++ src/OperatoryMatematyczneRef.java
@@ -0,0 +1,30 @@
+public class OperatoryMatematyczneRef{
+
+    public static void main(String[] args) {
+        int number1 = 4;
+        int number2 = 6;
+
+        int addition = number1+number2;
+        int substraction = number1-number2;
+        int multiplication = number1*number2;
+        int division = number1/number2;
+        int mod = number2%number1;
+
+        System.out.println("Dodawanie: " + addition);
+        System.out.println("Odejmowanie: " + substraction);
+        System.out.println("Dzielenie: " + division);
+        System.out.println("Mnozenie: " + multiplication);
+        System.out.println("Modulo: " + mod);
+
+        number1+=number2;
+        System.out.println("po dodaniu: " + number1);
+        number1-=number2;
+        System.out.println("po odjeciu: " + number1);
+        number1*=number2;
+        System.out.println("po mnozeniu: " + number1);
+        number1/=number2;
+        System.out.println("po dzieleniu: " + number1);
+        number1%=number2;
+        System.out.println("po modulo: " + number1);
+    }
+}
