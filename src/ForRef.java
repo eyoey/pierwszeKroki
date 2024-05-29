749093cb9a52cc0c1dce4dd9144419bd6b985628
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-05-29 14:49:06 +0200

    pętle

---
+++ src/ForRef.java
@@ -0,0 +1,20 @@
+public class ForRef {
+    public static void main(String[] args) {
+
+        int number = 100;
+        // i specjalna zmienna sterująca
+        for (int i = 0; i < number; i = i + 2) {
+            System.out.println(i);
+        }
+
+        // i specjalna zmienna sterująca
+        for (int i = 0; i < number;i++) {
+            if(i%5==0)
+                System.out.println(i);
+        }
+
+        for (int j = 0; j < number;j++) {
+            System.out.println("Będę robił prace domowe");
+        }
+    }
+}
