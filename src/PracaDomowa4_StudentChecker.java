d90dcc3730e2415ca77716f4ec9f47f90ef3c2d8
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-06-05 11:50:47 +0200

    praca domowa 4 update

--- src/PracaDomowa4_StudentChecker.java
+++ src/PracaDomowa4_StudentChecker.java
@@ -5,42 +5,44 @@
 public class PracaDomowa4_StudentChecker {
 
     public static void main(String[] args) {
-        Student Student1 = new Student();
-        Student1.imie = "S1_imie";
-        Student1.nazwisko = "S1_nazwisko";
-        Student1.email = "S1_email";
-        Student1.numerIndeksu = 1;
-        Student1.login = "S1_login";
+        Student student1 = new Student();
+        student1.imie = "S1_imie";
+        student1.nazwisko = "S1_nazwisko";
+        student1.email = "S1_email";
+        student1.numerIndeksu = 1;
+        student1.login = "S1_login";
 
-        Student Student2 = new Student();
-        Student2.imie = "S2_imie";
-        Student2.nazwisko = "S2_nazwisko";
-        Student2.email = "S2_email";
-        Student2.numerIndeksu = 2;
-        Student2.login = "S2_login";
+        Student student2 = new Student();
+        student2.imie = "S2_imie";
+        student2.nazwisko = "S2_nazwisko";
+        student2.email = "S2_email";
+        student2.numerIndeksu = 2;
+        student2.login = "S2_login";
 
-        Student Student3 = new Student();
-        Student3.imie = "S3_imie";
-        Student3.nazwisko = "S3_nazwisko";
-        Student3.email = "S3_email";
-        Student3.numerIndeksu = 3;
-        Student3.login = "S3_login";
+        Student student3 = new Student();
+        student3.imie = "S3_imie";
+        student3.nazwisko = "S3_nazwisko";
+        student3.email = "S3_email";
+        student3.numerIndeksu = 3;
+        student3.login = "S3_login";
 
-      /*  Student[] studenci = new Student[3];
-        studenci[0] = Student1;
-        studenci[1] = Student2;
-        studenci[2] = Student3;
+        Student[] studenci = new Student[3];
+        studenci[0] = student1;
+        studenci[1] = student2;
+        studenci[2] = student3;
 
         for (int i = 0; i < studenci.length; i++) {
-            studenci[i].daneStudenta();
-
+            studenci[i].przedstawSie();
+            studenci[i].podajEmail();
+            studenci[i].podajNrIndeksu();
+            studenci[i].zalogujSie();
         }
 
-        Student1.daneStudenta();
-        Student2.daneStudenta();
-        Student3.daneStudenta();*/
+       /* student1.daneStudenta();
+        student2.daneStudenta();
+        student3.daneStudenta();*/
 
-        String [] studenci = new String[3];
+        /*String [] studenci = new String[3];
         studenci[0] = Student1.daneStudenta2();
         studenci[1] = Student2.daneStudenta2();
         studenci[2] = Student3.daneStudenta2();
@@ -54,7 +56,7 @@ public class PracaDomowa4_StudentChecker {
 
             System.out.println(studenci[i]);
 
-        }
+        }*/
 
     }
 
