84e75ba37db1b700bf1527bdbe0e85582fd9b802
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-06-06 09:19:49 +0200

    homework 5 formatted

--- src/PracaDomowa_5_calc_v2.java
+++ src/PracaDomowa_5_calc_v2.java
@@ -4,8 +4,6 @@ public class PracaDomowa_5_calc_v2 {
 
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-
         Scanner scanner = new Scanner (System.in);
         System.out.println("Podaj kolejno 2 liczby");
         int number1 = scanner.nextInt();
@@ -13,23 +11,9 @@ public class PracaDomowa_5_calc_v2 {
         System.out.println("podaj kolejno 2 liczby dla dzielenia");
         float number3 = scanner.nextFloat();
         float number4 = scanner.nextFloat();
-        //float number3 = scanner.nextInt();
-       // System.out.println("Kwadrat pierwszej liczby to " + number1 * number1 + " !");
-       // System.out.println("Kwadrat drugiej liczby to " + number2 * number2 + " !");
 
-        //pierwszy sposób
-//        number1+=number2;
-//        System.out.println("po dodaniu: " + number1);
-//        number1-=number2;
-//        System.out.println("po odjeciu: " + number1);
-//        number1*=number2;
-//        System.out.println("po mnozeniu: " + number1);
-//        number1/=number2;
-//        System.out.println("po dzieleniu: " + number1);
-//        number1%=number2;
-//        System.out.println("po modulo: " + number1);
+        Calculator calculator = new Calculator();
 
-        //drugi sposob
         int addition = calculator.add(number1, number2);
         int subtraction = calculator.subtraction(number1, number2);
         int multiplication = calculator.multiplication(number1, number2);
