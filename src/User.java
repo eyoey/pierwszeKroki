8080a0351f5affa743dd9a31552e4efba813cd5d
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-06-17 12:16:40 +0200

    user konstructors

---
+++ src/User.java
@@ -0,0 +1,24 @@
+import javax.sound.midi.Soundbank;
+
+public class User {
+
+    public String username;
+    public String password;
+
+
+    public User() {
+        //System.out.println("z konstruktora");
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        System.out.println("Z konstruktora hello " + " " + username);
+
+    }
+
+    public void sayHEllo() {
+        System.out.println("Hello my name is " +  username);
+
+    }
+}
