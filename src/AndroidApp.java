8de6b33b439af61856e6fabba1b9c35d68ce7950
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-06-18 13:53:32 +0200

    homework 7 - appchecker

---
+++ src/AndroidApp.java
@@ -0,0 +1,14 @@
+public class AndroidApp extends App {
+
+    // public static String version;
+
+    public AndroidApp(String name) {
+        super(name);
+    }
+
+    public void runAndroidApp() {
+        System.out.println("running from android device");
+    }
+
+
+}
---
+++ src/IphoneApp.java
@@ -0,0 +1,13 @@
+public class IphoneApp extends App{
+
+    //public static String version;
+
+    public IphoneApp(String name) {
+        super(name);
+    }
+
+    public void runIphoneApp () {
+        System.out.println("running form iphone device");
+    }
+
+}
