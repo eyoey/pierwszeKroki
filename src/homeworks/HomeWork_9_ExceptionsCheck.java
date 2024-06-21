b167944af21402dffbbe4e59432d5a959c635292
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-06-21 10:26:37 +0200

    homework 9 exception check

--- src/homeworks/HomeWork_9_ExceptionsCheck.java
+++ src/homeworks/HomeWork_9_ExceptionsCheck.java
@@ -5,14 +5,13 @@ import Package1.User;
 
 public class HomeWork_9_ExceptionsCheck {
 
-    // pobierz wiek od użytkownika, jeżeli  będzie mniejszy od zera to wyrzuc wyjątek InvalidAgeException
-    // jeżeli mamy >=18 to możemy wypisać że użytkownik jest pełnoletni
+    // pobierz wiek od użytkownika, jeżeli będzie mniejszy od zera to wyrzuć wyjątek InvalidAgeException
+    // Jeżeli mamy >= 18 to możemy wypisać, że użytkownik jest pełnoletni
 
 
     public static void main(String[] args) throws InvalidAgeException {
         User user1 = new User("name", "pass", -1);
         int age = user1.age;
-
 
         if(age<0) {
             //System.out.println("age not valid");
