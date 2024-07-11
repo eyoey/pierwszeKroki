483c3394c65793532815e1b3abe5025733c2c9c3
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-07-11 14:44:27 +0200

    seleniumtest

---
+++ src/Selenium/SeleniumTest.java
@@ -0,0 +1,13 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
